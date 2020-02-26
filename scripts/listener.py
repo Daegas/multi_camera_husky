818c2952ef5505ef5182643a116723e28f8f779a
Daegas <darae.gs@gmail.com>
2020-02-26 15:05:10 -0600

    Read and save image

--- scripts/listener.py
+++ scripts/listener.py
@@ -2,29 +2,46 @@
 # -*- coding: utf-8 -*-
 #Listener from husky's camera.
 
+import sys
 import rospy
-from std_msgs.msg import  CameraInfo Image
+from sensor_msgs.msg import  CameraInfo, Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 
 # Instantiate CvBridge
 bridge = CvBridge()
+#Variables
+NHus=int(sys.argv[1]) if len(sys.argv) > 1 else 1
+cams_rgb=[]
+cams_info=[]
+for i in range(NHus):
+    cams_rgb.append([])
+    cams_info.append([])
 
-def callback(msg):
-    print('Received an image!')
+
+def callback(msg, N):
     try:
         #Convert to openCV2
         cv2_img=bridge.imgmsg_to_cv2(msg,"bgr8")
     except CvBridgeError, e:
         print(e)
     else:
-        #Save image as a jpeg
-        print('Wunderwall!')
+        #Do whatever
+        cams_rgb[N]=cv2_img
+        Analize()        
+
+def Analize():
+    for n in len(cams_rgb):
+
+
 def main():
     rospy.init_node('camera_listener', anonymous=True)
-    image_topic = "/husky/camera1/image_raw"
-    sub_cam_info = rospy.Subscriber(image_topic, CameraInfo)
-    sub_rgb=rospy.Subscriber(image_topic,Image)
+
+    for n in range(NHus):
+        image_topic = "/husky" + str(n) + "/camera/rgb/"
+        sub_cam_info = rospy.Subscriber(image_topic +"image_raw", Image, callback,n)
+        #TODO:Implement camera_info
+        #sub_rgb=rospy.Subscriber(image_topic + "camera_info", CameraInfo)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
